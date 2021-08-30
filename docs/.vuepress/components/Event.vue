7ef37db54cadc78ebb73a994f1e062ca6a94f831
zoomze <ladygaga1057@163.com>
2021-08-30 19:51:53 +0800

    更新: 文档更新

---
+++ docs/.vuepress/components/Event.vue
@@ -0,0 +1,19 @@
+<template>
+  <div @click="handleOuterClick">
+    <p @click="handleInnerClick">
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleOuterClick(event) {
+      console.log('outer', event.target, event.currentTarget)
+    },
+    handleInnerClick(event) {
+      console.log('inner', event.target, event.currentTarget)
+    },
+  }
+}
+</script>
